180187545412c114ec2eac6079b72ddfa8f9e4dc
ghosttravislt <enochbaafi56@gmail.com>
2024-10-15 06:01:44 +0100

    first commit to DropEase

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="Assets/styles/styles.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="homepage_container gradient">
+  <header class="header_nav">
+      <nav class="navbar">
+        <div class="logo"><p>logo</p></div>
+        <ul class="navlinks">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <!-- <div class="menu"> <i class="fas fa-bars"></i></div> -->
+         <div class="user_links">
+           <ul>
+            <li><a href="#" class="login">Login</a></li>
+            <li><a href="#" class="sign-up">Sign up</a></li>
+           </ul>
+
+         </div>
+      </nav>
+    </header>
+ 
+    <div class="hero_text">
+      <p> Get your items <br> delivered with just <br>a few clicks!</p>
+    </div>
+    
+    <div class="text">
+     <p>Welcome to Dropease – your go-to solution for fast, reliable, and hassle-free pickup and delivery! Whether you're sending packages across town or receiving goods from local stores, we make the process effortless</p>
+    </div>
+
+    <button class="btn">Get Started</button>
+
+    <div class="hero-img">
+      <img src="Assets/Images/Streamlined Shipment Services.png" alt="" class="img-hero">
+    </div>
+
+
+
+
+
+
+  </div>
+ 
+
+</body>
+</html>
