dd4d61adda6bec2a6222bbbb590d56d9aa7475fa
ghosttravislt <enochbaafi56@gmail.com>
2024-10-30 15:28:27 +0000

    how it works section added

--- index.php
+++ index.php
@@ -45,5 +45,40 @@
   <!-- <div class="hero-img1">
       <img src="Assets/Images/vecteezy_maps-crop.png" alt="" class="img-hero1">
     </div> -->
+
+
+     <!-- How it works section  -->
+         <section class="How-it-works-section">
+              <p class="How-it-works-title">How it Works </p>
+              <p class="description-text">Check out this simple description on how DropEase works to make pick up and delivery a breeze</p>
+
+               <!-- steps illustration container  -->
+                   <div class="illustrations-container">
+                    <div class="illustration">
+                      <img src="Assets/Images/undraw_undraw_takingphoto_7ugi_-1-_74xa.svg" alt=""  class="taking-picture">
+                      <p class="steps"><span class="number">01</span>Take a Picture</p>
+                      <p class="steps-info">
+                        Take a picture of your product indicating the product name and location to be delivered as well as the porduct description.
+                      </p>
+                    </div>
+                    <div class="illustration">
+                      <img src="Assets/Images/Location search-pana (1).png" alt=""  class="location">
+                      <p class="steps"><span class="number">02</span>Set Location</p>
+                      <p class="steps-info">
+                        Take a picture of your product indicating the product name and location to be delivered as well as the porduct description.
+                      </p>
+                    </div>
+                    <div class="illustration">
+                      <img src="Assets/Images/Take Away-pana (1).png" alt=""  class="rider">
+                      <p class="steps"><span class="number">03</span>Delivery</p>
+                      <p class="steps-info">
+                        Take a picture of your product indicating the product name and location to be delivered as well as the porduct description.
+                      </p>
+                    </div>
+                   </div>
+         </section>
+
+
+          <!--  -->
 </body>
 </html>
