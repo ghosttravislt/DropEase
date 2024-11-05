31b7acc8fd6653e32a3abf97890b38cc3ba407a5
ghosttravislt <enochbaafi56@gmail.com>
2024-11-05 03:21:56 +0000

    image added the additional info section

--- index.php
+++ index.php
@@ -56,29 +56,71 @@
                    <div class="illustrations-container">
                     <div class="illustration">
                       <img src="Assets/Images/undraw_undraw_takingphoto_7ugi_-1-_74xa.svg" alt=""  class="taking-picture">
+                      <div class="steps-container">
                       <p class="steps"><span class="number">01</span>Take a Picture</p>
                       <p class="steps-info">
                         Take a picture of your product indicating the product name and location to be delivered as well as the porduct description.
                       </p>
+                      </div>
                     </div>
                     <div class="illustration">
                       <img src="Assets/Images/Location search-pana (1).png" alt=""  class="location">
+                      <div class="steps-container">
                       <p class="steps"><span class="number">02</span>Set Location</p>
                       <p class="steps-info">
                         Take a picture of your product indicating the product name and location to be delivered as well as the porduct description.
                       </p>
+                      </div>
                     </div>
                     <div class="illustration">
                       <img src="Assets/Images/Take Away-pana (1).png" alt=""  class="rider">
+                      <div class="steps-container">
                       <p class="steps"><span class="number">03</span>Delivery</p>
                       <p class="steps-info">
                         Take a picture of your product indicating the product name and location to be delivered as well as the porduct description.
                       </p>
+                      </div>
                     </div>
                    </div>
          </section>
 
 
-          <!--  -->
+          <!-- customer  ratitng  section   -->
+            <section class="custmomer-rating-container">
+
+             <div class="ratings">
+               <p class="number-rating">200+</p>
+                <p class="raitng-text">Happy customers</p>
+             </div>
+             <div class="ratings">
+               <p class="number-rating">100+</p>
+                <p class="raitng-text1">partnership</p>
+             </div>
+             <div class="ratings">
+               <p class="number-rating">5.9+</p>
+                <p class="raitng-text2">Customer ratings</p>
+             </div>
+             <div class="ratings">
+               <p class="number-rating"> 80+</p>
+                <p class="raitng-text3">social media ratings</p>
+             </div>
+                  
+
+            </section>
+
+
+            <!-- Additional info section -->
+
+            <section class="additional-info-container">
+                <img src="Assets/Images/dark green delivery design1.png" alt="" class="additional-info">
+
+                 <!-- additional info text  -->
+                  <div class="additional-info-text-container">
+                      <p class="additional-info-title">Dropease is here to make deliveries smoother than ever!</p>
+
+                      <p class="additional-info-text">At Dropease, we’re committed to simplifying your deliveries. Whether you need a one-time pickup or regular deliveries, we’ve got you covered with fast, secure, and affordable service.</p>
+                      <button class="sign-up-now">Sign Up Now</button>
+                  </div>
+            </section>
 </body>
 </html>
